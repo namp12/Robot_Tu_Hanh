8ffcca1d7a0395df8a99aaf81af29915a9136930
Hoang <duong31102005@gmail.com>
2026-07-22 19:06:08 +0700

    front_hc_sr04_pins_changed_to_gpio1_and_2

--- ROBOT_AI/include/PinMap.h
+++ ROBOT_AI/include/PinMap.h
@@ -23,8 +23,8 @@
 #define MOTOR_RR_LPWM 11
 
 //================ HC-SR04 =================
-#define HC_FRONT_TRIG 12
-#define HC_FRONT_ECHO 13
+#define HC_FRONT_TRIG 1
+#define HC_FRONT_ECHO 2
 #define HC_REAR_TRIG  14
 #define HC_REAR_ECHO  15
 
